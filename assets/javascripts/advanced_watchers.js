a3f8247f1cd80d716ae0593547d056e765b387a3
wookayin <wookayin@gmail.com>
2013-09-05 22:45:28 +0900

    Add a feature to select watchers by group
    As in the former plugin, we have a group selectbox and action buttons.
    Select or unselect the users in the selected group as watchers,
    and this change is synchronized to the autocomplete inputbox.

--- assets/javascripts/advanced_watchers.js
+++ assets/javascripts/advanced_watchers.js
@@ -1,29 +1,34 @@
 (function($, undefined) {
 
-	var AdvancedWatchers = {};
+	var AdvancedWatchers = {
+    AutoComplete: {},
+    ByGroup : {}
+  };
 
   // initialize plugin by adjusting position of the DOM object.
   $(document).ready(function() {
 	  $("#watchers-advanced").show().appendTo($("#watchers_form"));
 
-		AdvancedWatchers.init();
+		AdvancedWatchers.AutoComplete.init();
+		AdvancedWatchers.ByGroup.init();
   });
 
-	AdvancedWatchers.init = function() {
+	AdvancedWatchers.AutoComplete.init = function() {
+    var self = this;
 		this.$selectbox = $("#watchers-select");
 		this.$checkboxes = $("#watchers_inputs input[type=checkbox]");
 		this.$watchers_autocomplete = this.$selectbox.select2();
 
 		this.$watchers_autocomplete.change(function() {
-			AdvancedWatchers.syncToCheckboxes();
+			self.syncToCheckboxes();
 		});
 		$("#watchers_inputs").on('change', 'input[type=checkbox]', function() {
-			AdvancedWatchers.syncFromCheckboxes();
+			self.syncFromCheckboxes();
 		});
 	};
 
 	// synchronize select2 -> checkbox
-	AdvancedWatchers.syncToCheckboxes = function() {
+	AdvancedWatchers.AutoComplete.syncToCheckboxes = function() {
 		var self = this;
 		var selected_watcher_userids = self.$selectbox.val();
 		self.$checkboxes.prop('checked', false);
@@ -36,7 +41,7 @@
 	};
 
 	// synchronize checkbox -> select2
-	AdvancedWatchers.syncFromCheckboxes = function() {
+	AdvancedWatchers.AutoComplete.syncFromCheckboxes = function() {
 		var self = this;
 		var selected_watcher_userids = self.$selectbox.val();	// previously exist
 		var userid_exists = {};
@@ -58,5 +63,50 @@
 		selected_watcher_userids = $.grep(selected_watcher_userids, function(v, i) { return userid_exists[v]; });
 		self.$selectbox.val(selected_watcher_userids).trigger("change");
 	};
+
+
+  AdvancedWatchers.ByGroup.init = function() {
+    var self = this;
+    self.$el = $('#watchers-advanced > .watchers-advanced-by-group');
+		self.$checkboxes = $("#watchers_inputs input[type=checkbox]");
+
+    var sync = function() {
+      // TODO change and remove this function (use by event binding)
+			AdvancedWatchers.AutoComplete.syncFromCheckboxes();
+    };
+
+    // register event handlers for buttons
+    self.$el.find('.action-select-all').click(function() {
+      self.$checkboxes.prop('checked', true);
+      sync();
+    });
+    self.$el.find('.action-unselect-all').click(function() {
+      self.$checkboxes.prop('checked', false);
+      sync();
+    });
+
+    var setCheckboxesInTheGroup = function(isChecked) {
+      var usersInGroup = $('#watcher_multiple_group').val().split(',');
+      var userid_target = {};
+      for(var i = 0; i < usersInGroup.length; ++ i) {
+        userid_target[ usersInGroup[i] ] = true;
+      }
+
+      self.$checkboxes.each( function() {
+        var user_id = $(this).val();
+        if(userid_target[user_id]) $(this).prop('checked', isChecked);
+      });
+    };
+
+    self.$el.find('.action-select').click(function() {
+      setCheckboxesInTheGroup(true);
+      sync();
+    });
+    self.$el.find('.action-unselect').click(function() {
+      setCheckboxesInTheGroup(false);
+      sync();
+    });
+
+  };
 
 })(jQuery);	// Requires Redmine 2.1+
