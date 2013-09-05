2c9b67262d0d675a176294754b2292225893236a
wookayin <wookayin@gmail.com>
2013-09-06 00:21:12 +0900

    Normalize whitespaces (use ruby convention?)

--- assets/javascripts/advanced_watchers.js
+++ assets/javascripts/advanced_watchers.js
@@ -1,78 +1,79 @@
 (function($, undefined) {
 
-	var AdvancedWatchers = {
+  var AdvancedWatchers = {
     AutoComplete: {},
     ByGroup : {}
   };
 
   // initialize plugin by adjusting position of the DOM object.
   $(document).ready(function() {
-	  $("#watchers-advanced").show().appendTo($("#watchers_form"));
+    $("#watchers-advanced").show().appendTo($("#watchers_form"));
 
-		AdvancedWatchers.AutoComplete.init();
-		AdvancedWatchers.ByGroup.init();
+    AdvancedWatchers.AutoComplete.init();
+    AdvancedWatchers.ByGroup.init();
   });
 
-	AdvancedWatchers.AutoComplete.init = function() {
+  AdvancedWatchers.AutoComplete.init = function() {
     var self = this;
-		this.$selectbox = $("#watchers-select");
-		this.$checkboxes = $("#watchers_inputs input[type=checkbox]");
-		this.$watchers_autocomplete = this.$selectbox.select2();
+    this.$selectbox = $("#watchers-select");
+    this.$checkboxes = $("#watchers_inputs input[type=checkbox]");
+    this.$watchers_autocomplete = this.$selectbox.select2({
+    });
 
-		this.$watchers_autocomplete.change(function() {
-			self.syncToCheckboxes();
-		});
-		$("#watchers_inputs").on('change', 'input[type=checkbox]', function() {
-			self.syncFromCheckboxes();
-		});
-	};
+    this.$watchers_autocomplete.change(function() {
+      self.syncToCheckboxes();
+    });
+    $("#watchers_inputs").on('change', 'input[type=checkbox]', function() {
+      self.syncFromCheckboxes();
+    });
+  };
 
-	// synchronize select2 -> checkbox
-	AdvancedWatchers.AutoComplete.syncToCheckboxes = function() {
-		var self = this;
-		var selected_watcher_userids = self.$selectbox.val();
-		self.$checkboxes.prop('checked', false);
+  // synchronize select2 -> checkbox
+  AdvancedWatchers.AutoComplete.syncToCheckboxes = function() {
+    var self = this;
+    var selected_watcher_userids = self.$selectbox.val();
+    self.$checkboxes.prop('checked', false);
 
-		if(!selected_watcher_userids) selected_watcher_userids = [];
-		for(var i = 0; i < selected_watcher_userids.length; ++ i) {
-			var userid = selected_watcher_userids[i];
-			self.$checkboxes.filter("[value=" + userid + "]").prop('checked', true);
-		}
-	};
+    if(!selected_watcher_userids) selected_watcher_userids = [];
+    for(var i = 0; i < selected_watcher_userids.length; ++ i) {
+      var userid = selected_watcher_userids[i];
+      self.$checkboxes.filter("[value=" + userid + "]").prop('checked', true);
+    }
+  };
 
-	// synchronize checkbox -> select2
-	AdvancedWatchers.AutoComplete.syncFromCheckboxes = function() {
-		var self = this;
-		var selected_watcher_userids = self.$selectbox.val();	// previously exist
-		var userid_exists = {};
-		if(!selected_watcher_userids) selected_watcher_userids = [];
-		for(var i = 0; i < selected_watcher_userids.length; ++ i) {
-			userid_exists[ selected_watcher_userids[i] ] = true;
-		}
-		self.$checkboxes.each( function() {
-			var user_id = $(this).val();
-			if( $(this).is(':checked') ) {
-				// append if not exists
-				if(!userid_exists[user_id]) selected_watcher_userids.push(user_id);
-				userid_exists[user_id] = true;
-			} else {
-				// remove
-				userid_exists[user_id] = false;
-			}
-		});
-		selected_watcher_userids = $.grep(selected_watcher_userids, function(v, i) { return userid_exists[v]; });
-		self.$selectbox.val(selected_watcher_userids).trigger("change");
-	};
+  // synchronize checkbox -> select2
+  AdvancedWatchers.AutoComplete.syncFromCheckboxes = function() {
+    var self = this;
+    var selected_watcher_userids = self.$selectbox.val();  // previously exist
+    var userid_exists = {};
+    if(!selected_watcher_userids) selected_watcher_userids = [];
+    for(var i = 0; i < selected_watcher_userids.length; ++ i) {
+      userid_exists[ selected_watcher_userids[i] ] = true;
+    }
+    self.$checkboxes.each( function() {
+      var user_id = $(this).val();
+      if( $(this).is(':checked') ) {
+        // append if not exists
+        if(!userid_exists[user_id]) selected_watcher_userids.push(user_id);
+        userid_exists[user_id] = true;
+      } else {
+        // remove
+        userid_exists[user_id] = false;
+      }
+    });
+    selected_watcher_userids = $.grep(selected_watcher_userids, function(v, i) { return userid_exists[v]; });
+    self.$selectbox.val(selected_watcher_userids).trigger("change");
+  };
 
 
   AdvancedWatchers.ByGroup.init = function() {
     var self = this;
     self.$el = $('#watchers-advanced > .watchers-advanced-by-group');
-		self.$checkboxes = $("#watchers_inputs input[type=checkbox]");
+    self.$checkboxes = $("#watchers_inputs input[type=checkbox]");
 
     var sync = function() {
       // TODO change and remove this function (use by event binding)
-			AdvancedWatchers.AutoComplete.syncFromCheckboxes();
+      AdvancedWatchers.AutoComplete.syncFromCheckboxes();
     };
 
     // register event handlers for buttons
@@ -109,4 +110,4 @@
 
   };
 
-})(jQuery);	// Requires Redmine 2.1+
+})(jQuery);  // Requires Redmine 2.1+
